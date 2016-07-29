5b0ad805bf6e76bc96007f83a160b622a00608d3
Gautam Marya <marya.gautam@gmail.com>
2016-07-29 17:10:27 +0530

    Added TSV Processing in Spark and Flink

---
+++ dataingest-spark/src/main/java/com/xavient/dataingest/spark/util/ProcessSTSVStream.java
@@ -0,0 +1,29 @@
+package com.xavient.dataingest.spark.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessSTSVStream {
+
+	public List<Object> tsvElements = new ArrayList<Object>();
+
+	public List<List<Object>> tsvData = new ArrayList<List<Object>>();
+
+	public List<List<Object>> getTSVData(String tsvString) {
+
+		try {
+
+			String parts[] = tsvString.split("\t");
+
+			for (String items : parts) {
+				tsvElements.add(items);
+
+			}
+			tsvData.add(tsvElements);
+
+		} catch (Exception e) {
+
+		}
+		return tsvData;
+	}
+}
